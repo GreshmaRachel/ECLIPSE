0175b97d02c1d407c0577698c095a989e33f5e9e
GreshmaRachel <greshma.shaji@mphasis.com>
2019-05-30 04:16:52 -0700

    commit

---
+++ Demo/src/Employee.java
@@ -0,0 +1,23 @@
+
+public class Employee {
+	private int empid;
+	private int empname;
+	public Employee(int empid, int empname) {
+		super();
+		this.empid = empid;
+		this.empname = empname;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public int getEmpname() {
+		return empname;
+	}
+	public void setEmpname(int empname) {
+		this.empname = empname;
+	}
+
+}
